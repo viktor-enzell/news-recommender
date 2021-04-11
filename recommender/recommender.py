a446a8d793e5b63cb61d6a1963808b2679fda88f
viktor-enzell <viktor.enzell@gmail.com>
2021-04-11 21:10:08 +0200

    Added dummy functions to recommender for communication with views.

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -3,5 +3,51 @@ class Recommender:
     Recommendation engine. Recommend articles stored in ES.
     """
 
+    def get_users(self):
+        available_users = (('user_0', 'No user selected'), ('user_1', 'User 1'), ('user_2', 'User 2'),)
+        return available_users
+
+    def get_random_articles(self):
+        articles = [
+            {
+                'title': 'Random article 1',
+                'id': '1',
+                'url': 'https://omni.se/'
+            },
+            {
+                'title': 'Random article 2',
+                'id': '2',
+                'url': 'https://omni.se/'
+            }
+        ]
+        return articles
+
     def recommend_articles(self, user_id):
-        return []
+        articles = []
+
+        if user_id == 'user_1':
+            articles = [
+                {
+                    'title': 'An article recommended for User 1',
+                    'id': '3',
+                    'url': 'https://omni.se/'
+                },
+                {
+                    'title': 'Another article recommended for User 1',
+                    'id': '4',
+                    'url': 'https://omni.se/'
+                }
+            ]
+        elif user_id == 'user_2':
+            articles = [
+                {
+                    'title': 'Some article recommended for User 2',
+                    'id': '5',
+                    'url': 'https://omni.se/'
+                }
+            ]
+
+        return articles
+
+    def like_article(self, user_id, article_id):
+        print(f'User {user_id} likes article {article_id}')
