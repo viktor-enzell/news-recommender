8a29d293e4dfb6d67b1cfaf6b406dbc3c40d965e
viktor-enzell <viktor.enzell@gmail.com>
2021-04-11 21:09:19 +0200

    Views reformatted and updated for handling likes.

--- recommender/forms.py
+++ recommender/forms.py
@@ -3,16 +3,14 @@ from django import forms
 
 class UserForm(forms.Form):
     def __init__(self, *args, **kwargs):
-        initial_user = kwargs.get('initial_user')
-        if initial_user is not None:
-            kwargs.pop('initial_user')
+        initial_user = kwargs.pop('initial_user', None)
+        available_users = kwargs.pop('available_users', None)
         super(UserForm, self).__init__(*args, **kwargs)
         if initial_user is not None:
             self.fields['user'].initial = initial_user
-
-    USERS = (('User 1', 'User 1'), ('User 2', 'User 2'),)
+        if available_users is not None:
+            self.fields['user'].choices = available_users
 
     user = forms.ChoiceField(
-        choices=USERS,
         widget=forms.Select(attrs={'class': 'form-select'})
     )
--- recommender/views.py
+++ recommender/views.py
@@ -1,49 +1,53 @@
 from django.shortcuts import render
-from .forms import UserForm
 from django.http import HttpResponseRedirect
+
+from .forms import UserForm
+from .recommender import Recommender
+
+# Not sure if having recommender as a separate class is the "Django way of doing things".
+# Not sure if this is the proper place to instantiate the recommender either.
+recommender = Recommender()
 
 
 def index(request):
     """
-    TODO:
-        instantiate recommender somewhere
-        get users from recommender
-        get random news from recommender
-        send users and news to UI
-        retrieve selected user from UI
-        keep track of a mapping between session and user
-        send updated news to UI
-        retrieve feedback from UI and pass to recommender
-        send updated news to UI
+    Handling all GET and POST requests.
     """
-    articles = [
-        {
-            'title': 'Article 1',
-            'url': 'https://omni.se/'
-        },
-        {
-            'title': 'Article 2',
-            'url': 'https://omni.se/'
+    available_users = recommender.get_users()
+
+    if request.method == 'GET':
+        user = request.session.get('user', None)
+
+        if user and user != 'user_0':
+            # If user is already selected, return recommended articles
+            articles = recommender.recommend_articles(user)
+            form = UserForm(initial_user=user, available_users=available_users)
+        else:
+            # If user is not selected, return a set of random articles.
+            # Maybe return no articles before user is selected instead?.
+            articles = recommender.get_random_articles()
+            form = UserForm(available_users=available_users)
+
+        context = {
+            'form': form,
+            'articles': articles
         }
-    ]
+        return render(request, 'index.html', context)
 
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = UserForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            data = form.cleaned_data
-            if data.get('user'):
-                request.session['user'] = data.get('user')
-                return HttpResponseRedirect('/')
+        if request.POST.get('select_user', False):
+            # Handle post request for selecting user
+            form = UserForm(request.POST, available_users=available_users)
+            if form.is_valid():
+                data = form.cleaned_data
+                if data.get('user'):
+                    request.session['user'] = data.get('user')
 
-    if request.session.get('user', False):
-        form = UserForm(initial_user=request.session.get('user', False))
-    else:
-        form = UserForm()
+        elif request.POST.get('like_article', False):
+            # Handle post request for user liking article
+            user = request.session['user']
+            article = request.POST.get('like_article')
+            recommender.like_article(user, article)
 
-    context = {
-        'form': form,
-        'articles': articles
-    }
-    return render(request, 'index.html', context)
+        # Redirect back to / so that a GET request is sent and the articles are updated
+        return HttpResponseRedirect('/')
