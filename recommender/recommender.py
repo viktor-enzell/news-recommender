bc643806c45f170b92a4621e87e5613431712370
Tim Jonsson <timjonss@kth.se>
2021-04-16 11:14:07 +0200

    Added search function

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -1,7 +1,31 @@
+from elasticsearch import Elasticsearch
+
 class Recommender:
     """
     Recommendation engine. Recommend articles stored in ES.
     """
+    def __init__(self):
+        self.elastic_client = Elasticsearch(hosts=["localhost"])
 
-    def recommend_articles(self, user_id):
-        return []
+    def recommend_articles(self, user_id, query, search_type):
+        return self.search(query, search_type)
+
+    def search(self, query, search_type):
+        # Constructs simple query
+        matches = []
+        for s in query.split(" "):
+            matches.append({ "match": { "text": s}})
+        
+        body = {
+                "query": {
+                    "bool": {
+                        search_type: matches
+                        }
+                    }
+                }
+        res = self.elastic_client.search(index="scrapy", body=body)
+        return res['hits']['hits']
+        
+
+
+
